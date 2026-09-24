ea62a40e1471e47d910ef8b8cab5fed2f4b66536
Marco <marcoantonioca47@gmail.com>
2026-09-24 20:03:00 -0300

    Forcar atualizacao da logo no PWA e cache v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bv-lanches-pwa-v2';
+const CACHE_NAME='bv-lanches-pwa-v3';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
