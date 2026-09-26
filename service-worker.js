5b9192263f45339d834d127bc0b9abce8ca59e50
Marco <marcoantonioca47@gmail.com>
2026-09-26 08:31:56 -0300

    chore: renovar cache PWA do BV Lanches

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bv-lanches-pwa-v16';
+const CACHE_NAME='bv-lanches-pwa-v17';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
