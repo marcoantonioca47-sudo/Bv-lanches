15bf8b3d42d7c3f40accf799d22c53850f3c0e83
Marco <marcoantonioca47@gmail.com>
2026-09-26 09:03:24 -0300

    Atualiza cache apos ajuste do Refri Mini

--- bv-pwa-v1.js
+++ bv-pwa-v1.js
@@ -57,7 +57,7 @@
     });
     window.addEventListener('load',async()=>{
       try{
-        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.982',{scope:'./'});
+        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.983',{scope:'./'});
         await reg.update();
         hadController=hadController||!!navigator.serviceWorker.controller;
         if(updateTimer)clearInterval(updateTimer);
