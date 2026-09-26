b0b98214299bf51e0aafbfeb39d0fbc6f88c7984
Marco <marcoantonioca47@gmail.com>
2026-09-26 09:29:50 -0300

    Forca atualizacao do PWA apos correcao do Refri Mini

--- bv-pwa-v1.js
+++ bv-pwa-v1.js
@@ -57,7 +57,7 @@
     });
     window.addEventListener('load',async()=>{
       try{
-        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.1003',{scope:'./'});
+        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.1005',{scope:'./'});
         await reg.update();
         hadController=hadController||!!navigator.serviceWorker.controller;
         if(updateTimer)clearInterval(updateTimer);
