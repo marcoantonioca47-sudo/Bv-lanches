500e91b520fa45851c90c90505279c74a9879660
Marco <marcoantonioca47@gmail.com>
2026-09-26 09:12:15 -0300

    Atualiza cache Refri Mini 1001

--- bv-pwa-v1.js
+++ bv-pwa-v1.js
@@ -57,7 +57,7 @@
     });
     window.addEventListener('load',async()=>{
       try{
-        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.1000',{scope:'./'});
+        const reg=await navigator.serviceWorker.register('./service-worker.js?v=20260926.1001',{scope:'./'});
         await reg.update();
         hadController=hadController||!!navigator.serviceWorker.controller;
         if(updateTimer)clearInterval(updateTimer);
