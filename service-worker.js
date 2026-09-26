2db4862ea706d6af35a40b75597b6d2841c61668
Marco <marcoantonioca47@gmail.com>
2026-09-26 09:03:28 -0300

    Atualiza cache apos ajuste do Refri Mini

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='bv-lanches-pwa-v19';
+const CACHE_NAME='bv-lanches-pwa-v20';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
